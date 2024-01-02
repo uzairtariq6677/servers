cf0401fa25fbe93e081ebd92aee91229e795237c
uzairtariq6677 <uzairtariq6677@gmail.com>
2024-01-02 09:56:56 +0500

    initial commit

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-
 const express=require('express');
 const app=express();
 app.get('/',(req,res)=>{
-    res.send({hi:'there'});
+    res.send({hi:'there'})
 })
 app.listen(5000);
-const Port=process.env.Port||5000;
-app.listen(Port);
